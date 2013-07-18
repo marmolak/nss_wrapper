f5b55401ef67999114ac8d2bd566da292544596c
Andreas Schneider <asn@cryptomilk.org>
2013-07-18 11:50:51 +0200

    tests: Add getnameinfo() test with passing NULL pointers.

--- tests/test_getnameinfo.c
+++ tests/test_getnameinfo.c
@@ -194,6 +194,55 @@ static void test_nwrap_getnameinfo_local(void **state)
 	assert_string_equal(serv, "ssh");
 }
 
+static void test_nwrap_getnameinfo_null(void **state)
+{
+	char host[256] = {0};
+	char serv[256] = {0};
+	struct sockaddr_in sin;
+	struct sockaddr_in6 sin6;
+	int flags = 0;
+	int rc;
+
+	(void) state; /* unused */
+
+	rc = getnameinfo(NULL,
+			 0,
+			 host, sizeof(host),
+			 serv, sizeof(serv),
+			 flags);
+	assert_int_equal(rc, EAI_FAMILY);
+
+	/* IPv4 */
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(22);
+	rc = inet_pton(AF_INET, "127.0.0.11", &sin.sin_addr);
+	assert_int_equal(rc, 1);
+
+	rc = getnameinfo((const struct sockaddr *)&sin,
+			 sizeof(struct sockaddr_in),
+			 NULL, 0,
+			 serv, sizeof(serv),
+			 flags);
+	assert_int_equal(rc, 0);
+
+	assert_string_equal(serv, "ssh");
+
+	/* IPv6 */
+	sin6.sin6_family = AF_INET6;
+	sin6.sin6_port = htons(22);
+	rc = inet_pton(AF_INET6, "::13", &sin6.sin6_addr);
+	assert_int_equal(rc, 1);
+
+	rc = getnameinfo((const struct sockaddr *)&sin6,
+			 sizeof(struct sockaddr_in6),
+			 host, sizeof(host),
+			 NULL, 0,
+			 flags);
+	assert_int_equal(rc, 0);
+
+	assert_string_equal(host, "beteigeuze.galaxy.site");
+}
+
 int main(void) {
 	int rc;
 
@@ -202,6 +251,7 @@ int main(void) {
 		unit_test(test_nwrap_getnameinfo_numeric),
 		unit_test(test_nwrap_getnameinfo_any),
 		unit_test(test_nwrap_getnameinfo_local),
+		unit_test(test_nwrap_getnameinfo_null),
 	};
 
 	rc = run_tests(tests);
