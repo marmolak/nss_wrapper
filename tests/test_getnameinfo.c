869b7ec00c1da9a2c73080f7b284f1e39f2cf89a
Andreas Schneider <asn@cryptomilk.org>
2013-07-18 11:42:02 +0200

    tests: Add getnameinfo() test for "any" addresses.

--- tests/test_getnameinfo.c
+++ tests/test_getnameinfo.c
@@ -106,12 +106,57 @@ static void test_nwrap_getnameinfo_numeric(void **state)
 	assert_string_equal(serv, "53");
 }
 
+static void test_nwrap_getnameinfo_any(void **state)
+{
+	char host[256] = {0};
+	char serv[256] = {0};
+	struct sockaddr_in sin;
+	struct sockaddr_in6 sin6;
+	int flags = 0;
+	int rc;
+
+	(void) state; /* unused */
+
+	/* IPv4 */
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(22);
+	rc = inet_pton(AF_INET, "0.0.0.0", &sin.sin_addr);
+	assert_int_equal(rc, 1);
+
+	rc = getnameinfo((const struct sockaddr *)&sin,
+			 sizeof(struct sockaddr_in),
+			 host, sizeof(host),
+			 serv, sizeof(serv),
+			 flags);
+	assert_int_equal(rc, 0);
+
+	assert_string_equal(host, "0.0.0.0");
+	assert_string_equal(serv, "ssh");
+
+	/* IPv6 */
+	sin6.sin6_family = AF_INET6;
+	sin6.sin6_port = htons(22);
+	rc = inet_pton(AF_INET6, "::", &sin6.sin6_addr);
+	assert_int_equal(rc, 1);
+
+	rc = getnameinfo((const struct sockaddr *)&sin6,
+			 sizeof(struct sockaddr_in6),
+			 host, sizeof(host),
+			 serv, sizeof(serv),
+			 flags);
+	assert_int_equal(rc, 0);
+
+	assert_string_equal(host, "::");
+	assert_string_equal(serv, "ssh");
+}
+
 int main(void) {
 	int rc;
 
 	const UnitTest tests[] = {
 		unit_test(test_nwrap_getnameinfo),
 		unit_test(test_nwrap_getnameinfo_numeric),
+		unit_test(test_nwrap_getnameinfo_any),
 	};
 
 	rc = run_tests(tests);
