150c0233464798d25912ba04a8d39646c0ca04dd
Andreas Schneider <asn@cryptomilk.org>
2013-07-23 14:48:30 +0200

    cmake: Fix drop location.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,5 +5,5 @@ set(CTEST_NIGHTLY_START_TIME "01:00:00 CET")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "mock.cryptomilk.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=nss_wrapper")
+set(CTEST_DROP_LOCATION "/submit.php?project=nsswrapper")
 set(CTEST_DROP_SITE_CDASH TRUE)
