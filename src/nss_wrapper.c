a4c9907ec0f9379cf1ef02cf00947b806c031abb
Andreas Schneider <asn@samba.org>
2013-12-17 12:07:48 +0100

    nwrap: Add libc_entgrent().

--- src/nss_wrapper.c
+++ src/nss_wrapper.c
@@ -855,6 +855,13 @@ static int libc_getgrent_r(struct group *group,
 #endif /* HAVE_SOLARIS_GETGRENT_R */
 #endif /* HAVE_GETGRENT_R */
 
+static void libc_endgrent(void)
+{
+	nwrap_load_lib_function(NWRAP_LIBC, endgrent);
+
+	nwrap_main_global->libc->fns->_libc_endgrent();
+}
+
 /*********************************************************
  * NWRAP NSS MODULE LOADER FUNCTIONS
  *********************************************************/
@@ -1066,8 +1073,6 @@ static void nwrap_libc_init(struct nwrap_main *r)
 	/* Load symbols of libc */
 	handle = r->libc->handle;
 
-	*(void **) (&r->libc->fns->_libc_endgrent) =
-		nwrap_libc_fn(handle, "endgrent");
 #ifdef HAVE_GETGROUPLIST
 	*(void **) (&r->libc->fns->_libc_getgrouplist) =
 		nwrap_libc_fn(handle, "getgrouplist");
@@ -3428,7 +3433,7 @@ static void nwrap_endgrent(void)
 void endgrent(void)
 {
 	if (!nwrap_enabled()) {
-		nwrap_main_global->libc->fns->_libc_endgrent();
+		libc_endgrent();
 		return;
 	}
 
